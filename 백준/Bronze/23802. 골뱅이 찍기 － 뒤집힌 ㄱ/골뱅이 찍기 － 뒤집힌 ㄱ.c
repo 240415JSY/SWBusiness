65bef2387e8d8a04e28dfa8027d8369e82eee7f9
ForeverRUi <77910158+ForeverRUi@users.noreply.github.com>
2024-11-11 20:48:56 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㄱ, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/23802. 골뱅이 찍기 － 뒤집힌 ㄱ/골뱅이 찍기 － 뒤집힌 ㄱ.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N * 5; j++){
+            printf("@");
+        }
+        printf("\n");
+    }
+    for(int i = 0; i < N * 4; i++){
+        for(int j = 0; j < N; j++){
+            printf("@");
+        }
+        printf("\n");
+    }
+    return 0;
+}
