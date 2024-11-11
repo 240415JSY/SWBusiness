6e0a024bb9b37c3dcccc1bf19b29a25d000df8b8
ForeverRUi <77910158+ForeverRUi@users.noreply.github.com>
2024-11-11 20:34:37 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b, A, B;
+    scanf("%d\n", &a);
+    for (b = 1; b <= a; b++){
+        scanf("%d %d", &A, &B);
+        printf("Case #%d: %d + %d = %d\n", b, A, B, A + B);
+    }
+    return 0;
+}
